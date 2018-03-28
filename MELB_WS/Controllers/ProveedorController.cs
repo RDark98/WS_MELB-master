9addd50d26c9a9f1a93b56468c623a60e1b66568
YaderDavid <33787527+YaderDavid@users.noreply.github.com>
2018-03-27 23:31:40 -0600

    Proveedor.cs , ProveedorController.cs
    Creacion de Clases .cs

---
+++ MELB_WS/Controllers/ProveedorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using MELB_WS.Models.Inventario.Operaciones;
+
+namespace MELB_WS.Controllers
+{
+    public class ProveedorController : ApiController
+    {
+        Operaciones_Inventario Instancia_OP = new Operaciones_Inventario();
+
+      
+    }
+}
---
+++ MELB_WS/Models/Inventario/Modelos/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MELB_WS.Models.Inventario.Modelos
+{
+    public class Proveedor
+    {
+        public int ID_Proveedor { get; set; }
+        public string Nombre { get; set; }
+        public string Material { get; set; }
+        public int Telefono_1 { get; set; }
+        public int Telefono_2 { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public string Imagen { get; set; }
+    }
+}
