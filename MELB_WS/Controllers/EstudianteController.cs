8cb38d4e030f34604ec474b5cc2a04fe43349848
YaderDavid <33787527+YaderDavid@users.noreply.github.com>
2018-03-29 01:44:28 -0600

    29-03-2018 01:43
    Creacion de Controladores Estuche y Estudiante

---
+++ MELB_WS/Controllers/EstudianteController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using MELB_WS.Models.Inventario.Operaciones;
+using MELB_WS.Models.Inventario.Modelos;
+
+namespace MELB_WS.Controllers
+{
+    public class EstudianteController : ApiController
+    {
+        Operaciones_Inventario Instancia_OP = new Operaciones_Inventario();
+
+        // Retorno de toda la coleccion de datos //
+        [SwaggerOperation("GetAll")]
+        public string Get()
+        {
+            return null;
+        }
+
+        // Creacion de un nuevo registro //
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Post([FromBody]Estudiante Ins)
+        {
+            return null;
+        }
+
+        // Actualizacion de un registro ya existente //
+        [SwaggerOperation("Update")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Put([FromBody]Estudiante Ins)
+        {
+            return null;
+        }
+
+        // Eliminación de un registro //
+        [SwaggerOperation("Delete")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Delete(int ID)
+        {
+            return null;
+        }
+
+    }
+}
